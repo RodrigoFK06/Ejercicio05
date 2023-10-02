e8899698e248e945f0489d104943c9f1094d64d9
Tony Torres <torrespastor4@hotmail.com>
2023-10-01 19:48:11 -0500

    ta bien

---
+++ Ejercicio05/aladar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio05
+{
+    class aladar
+    {
+        public string aladarcito;
+    }
+}
